1b9f3ac6a783ef625feff4d5d079deb05641b1f1
maribun20 <maribun23@gmail.com>
2021-03-06 00:13:05 +0700

    upload avxintrin-emu.h banner.h bench.cpp bignum.cpp build.cmd build.sh

---
+++ banner.h
@@ -0,0 +1,23 @@
+static void show_credits()
+{
+        printf(CL_LGR"     #####   #####  ##      ##  ###  ##      #  #####   #### \n");
+        printf(CL_LGR"    #       #       # #    # #   #   # #     #  #      #    #\n");
+        printf(CL_LGR"    #       #       #  #  #  #   #   #  #    #  #      #    #\n");
+        printf(CL_LGR"    #       #       #   ##   #   #   #   #   #  #####  #####\n");
+        printf(CL_LGR"    #       #       #        #   #   #    #  #  #      # #\n");
+        printf(CL_LGR"    #       #       #        #   #   #     # #  #      #  #\n");
+        printf(CL_LGR"     #####   ##### ##        ## ###  #      ##  ###### #   ###\n");
+        printf(CL_N "                           " CL_LYL2 "++ VERUS ++"  CL_N"\n");
+        printf("\n");
+        printf(CL_LCY"              **"PACKAGE_NAME""CL_LYL" "PACKAGE_VERSION CL_LCY" by maribun20@github**\n");
+        printf(CL_LYL"       Based Originaly from ccminer by tpruvot & monkins1010\n");
+        printf(CL_N"######################################################################\n\n");
+        printf(CL_LCY"  Author  "CL_LYL"           : Maribun20\n");
+        printf(CL_LCY"  Telagram"CL_LYL"           : @Aminudin1 (Wong Fi Hung)\n");
+        printf(CL_LCY"  Youtube Channel"CL_LYL"    : CHANNEL CRYPTO\n");
+        printf(CL_LCY"  Git repo"CL_LYL"           : https:" "/" "/" "github.com" "/" "maribun20" "/" "ccminer.git\n");
+        printf(CL_LCY"  Original git repo"CL_LYL"  : https:" "/" "/" "github.com" "/" "monkins1010" "/" "ccminer.git\n");
+        printf(CL_LCY"  VRSC donation addr"CL_LYL" : RRssATB2CkucceLg7CEgbzndZQzWaQgPdE (maribun20)\n");
+        printf(CL_LCY"  BTC donation addr "CL_LYL" : 1FhDPLPpw18X4srecguG3MxJYe4a1JsZnd (tpruvot)\n");
+        printf(CL_N"######################################################################\n");
+}
